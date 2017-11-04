1c0f4fa619c3d0b1bfadd0961b2eeaf4d42ea10f
Leonardo Correia <leonardo.correia.96@outlook.com>
2017-11-04 03:09:11 -0200

    draw "LEO" in screen like a sample

--- pipeAlign.main.asm
+++ pipeAlign.main.asm
@@ -8,10 +8,33 @@
 	.globl main
 			
 	main:		
-		jal fillBackgroundColor
+		jal fillBackgroundColor		
 					
-		sendParameters(3, 3, 62, 126, BLUE_COLOR, NOT_FILLED)		# start in (2,2) and finish in (63, 127)
-		
+		sendParameters(3, 3, 62, 126, BLUE_COLOR, NOT_FILLED)		# start in (2,2) and finish in (63,127)		
 		jal drawRectangle
-					
+		
+		# draw L
+		sendParameters(6, 10, 20, BLUE_COLOR)						# initial line, final line, block number, color		
+		jal drawVerticalLine		
+		
+		sendParameters(10, 21, 22, BLUE_COLOR)						# line number, initial block, final block, color		
+		jal drawHorizontalLine										
+		
+		# draw E
+		sendParameters(6, 10, 24, BLUE_COLOR)						# initial line, final line, block number, color		
+		jal drawVerticalLine		
+		
+		sendParameters(6, 25, 27, BLUE_COLOR)						# line number, initial block, final block, color		
+		jal drawHorizontalLine		
+		
+		sendParameters(8, 25, 27, BLUE_COLOR)						# line number, initial block, final block, color		
+		jal drawHorizontalLine										
+		
+		sendParameters(10, 25, 27, BLUE_COLOR)						# line number, initial block, final block, color		
+		jal drawHorizontalLine
+		
+		# draw O
+		sendParameters(6, 29, 10, 32, BLUE_COLOR, NOT_FILLED)		# start in (6,29) and finish in (10,32)		
+		jal drawRectangle
+								
 	return_EXIT_SUCCESS
