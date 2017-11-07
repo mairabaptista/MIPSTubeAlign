3fe213fc82d5841deb4aed8692f7cf2d5b7a365a
Leonardo Correia <leonardo.correia.96@outlook.com>
2017-11-07 19:01:41 -0200

    gameService and matrix adjusts

---
+++ pipeAlign.gameService.asm
@@ -0,0 +1,26 @@
+.data	
+	# Data Segment
+.text
+	createCursor:
+		#TODO
+	jr $ra
+		
+	moveCursorUp:
+		#TODO
+	jr $ra
+	
+	moveCursorDown:
+		#TODO
+	jr $ra
+		
+	moveCursorLeft:
+		#TODO
+	jr $ra
+	
+	moveCursorRight:
+		#TODO
+	jr $ra
+	
+	toggleTube:
+		#TODO
+	jr $ra
--- pipeAlign.main.asm
+++ pipeAlign.main.asm
@@ -1,62 +1,96 @@
 .include "pipeAlign.constants.asm"
 .include "pipeAlign.drawingService.asm"
 .include "pipeAlign.displayService.asm"
+.include "pipeAlign.gameService.asm"
 
 .data	
-	# Data Segment	
+	# Data Segment
 .text
 	.globl main
 			
 	main:		
 		jal fillBackgroundColor		
 				
+		# arguments: line number, color
+		sendParameters(10, BLUE_COLOR)
+		jal drawEntireLine
+				
 		# draw margin
-		sendParameters(12, 6, 126, 249, BLUE_COLOR, NOT_FILLED)		# start in (12,6) and finish in (126,249)		
-		jal drawRectangle
+		#sendParameters(12, 7, 126, 250, BLUE_COLOR, NOT_FILLED)		# start in (12,6) and finish in (126,249)		
+		#jal drawRectangle
 		#
 		
 		# horizontal		
-		sendParameters(12, 6, 31, 33, BLUE_COLOR, NOT_FILLED)		
-		jal drawRectangle
+		#sendParameters(12, 7, 31, 34, BLUE_COLOR, NOT_FILLED)		
+		#jal drawRectangle
 		
-		sendParameters(12, 33, 31, 60, BLUE_COLOR, NOT_FILLED)		
-		jal drawRectangle		
+		sendParameters(12, 7, 10, RED_COLOR)											# arguments: line number, initial block, final block, color
+		jal drawHorizontalLine
 		
-		sendParameters(12, 60, 31, 87, BLUE_COLOR, NOT_FILLED)		
+		sendParameters(12, 31, 34, RED_COLOR)											# arguments: line number, initial block, final block, color
+		jal drawHorizontalLine
+		
+		sendParameters(31, 7, 10, RED_COLOR)											# arguments: line number, initial block, final block, color
+		jal drawHorizontalLine
+		
+		sendParameters(31, 31, 34, RED_COLOR)											# arguments: line number, initial block, final block, color
+		jal drawHorizontalLine
+		
+		
+		
+		# arguments: initial line, final line, block number, color
+		sendParameters(12, 15, 7, RED_COLOR)											
+		jal drawVerticalLine
+		
+		sendParameters(12, 15, 34, RED_COLOR)										
+		jal drawVerticalLine
+		
+		sendParameters(28, 31, 7, RED_COLOR)											
+		jal drawVerticalLine
+		
+		sendParameters(28, 31, 34, RED_COLOR)											
+		jal drawVerticalLine
+		#
+				
+		
+		#sendParameters(12, 34, 31, 61, BLUE_COLOR, NOT_FILLED)		
+		#jal drawRectangle		
+		
+		sendParameters(12, 61, 31, 88, BLUE_COLOR, NOT_FILLED)		
 		jal drawRectangle
 				
-		sendParameters(12, 87, 31, 114, BLUE_COLOR, NOT_FILLED)		
+		sendParameters(12, 88, 31, 115, BLUE_COLOR, NOT_FILLED)		
 		jal drawRectangle
 				
-		sendParameters(12, 114, 31, 141, BLUE_COLOR, NOT_FILLED)	
+		sendParameters(12, 115, 31, 142, BLUE_COLOR, NOT_FILLED)	
 		jal drawRectangle
 		
-		sendParameters(12, 141, 31, 168, BLUE_COLOR, NOT_FILLED)	
+		sendParameters(12, 142, 31, 169, BLUE_COLOR, NOT_FILLED)	
 		jal drawRectangle
 				
-		sendParameters(12, 168, 31, 195, BLUE_COLOR, NOT_FILLED)	
+		sendParameters(12, 169, 31, 196, BLUE_COLOR, NOT_FILLED)	
 		jal drawRectangle
 				
-		sendParameters(12, 195, 31, 222, BLUE_COLOR, NOT_FILLED)	
+		sendParameters(12, 196, 31, 223, BLUE_COLOR, NOT_FILLED)	
 		jal drawRectangle
 		
-		sendParameters(12, 222, 31, 249, BLUE_COLOR, NOT_FILLED)	
+		sendParameters(12, 223, 31, 250, BLUE_COLOR, NOT_FILLED)	
 		jal drawRectangle	
 		
 		#vertical
-		sendParameters(31, 6, 50, 33, BLUE_COLOR, NOT_FILLED)		
-		jal drawRectangle
+		#sendParameters(31, 7, 50, 34, BLUE_COLOR, NOT_FILLED)		
+		#jal drawRectangle
 				
-		sendParameters(50, 6, 69, 33, BLUE_COLOR, NOT_FILLED)		
+		sendParameters(50, 7, 69, 34, BLUE_COLOR, NOT_FILLED)		
 		jal drawRectangle
 		
-		sendParameters(69, 6, 88, 33, BLUE_COLOR, NOT_FILLED)		
+		sendParameters(69, 7, 88, 34, BLUE_COLOR, NOT_FILLED)		
 		jal drawRectangle		
 		
-		sendParameters(88, 6, 107, 33, BLUE_COLOR, NOT_FILLED)		
+		sendParameters(88, 7, 107, 34, BLUE_COLOR, NOT_FILLED)		
 		jal drawRectangle
 				
-		sendParameters(107, 6, 126, 33, BLUE_COLOR, NOT_FILLED)		
+		sendParameters(107, 7, 126, 34, BLUE_COLOR, NOT_FILLED)		
 		jal drawRectangle
 									
 	return_EXIT_SUCCESS
