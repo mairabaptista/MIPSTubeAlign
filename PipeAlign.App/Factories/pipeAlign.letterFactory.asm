5ea78264f1be1926458fda35d17896c7b01fc66a
Fernando Soares <fernando.dos.santos@outlook.com>
2017-11-28 23:23:09 -0200

    Letters

--- PipeAlign.App/Factories/pipeAlign.letterFactory.asm
+++ PipeAlign.App/Factories/pipeAlign.letterFactory.asm
@@ -13,24 +13,32 @@
 
         move $t0, $v0
         move $t1, $v1
-        add $t0, $t0, 4     #margin             
+        add $t0, $t0, 3     #margin 
 
-        drawLeftVerticalLine:
-            beq $a1, 66, drawMiddleVerticalLine
-            beq $a1, 73, drawMiddleVerticalLine
-            beq $a1, 84, drawMiddleVerticalLine
+        pushInStack($ra, $t0, $t1)
+        pushInStack($a0, $a1)
+        sendParameters($t0, $t1)
+        jal getPositionFromBlock
+        popFromStack($a0, $a1)
+        popFromStack($ra, $t0, $t1)   
+        move $t7, $v0 
 
-            add $t2, $t0, 11                
-            add $t3, $t1, 1                 
-            pushInStack($ra, $t0, $t1)
-            pushInStack($a0, $a1)
-            sendParameters($t0, $t1, $t2, $t3, BORDER_COLOR, FILLED)
-            jal drawRectangle
-            popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
+
         
-        drawMiddleTopVerticalLine:          
-            
+        drawMiddleTopVerticalLine:
+
+            beq $a1, 65, drawTopHorizontalLine
+            beq $a1, 67, drawTopHorizontalLine
+            beq $a1, 69, drawTopHorizontalLine
+            beq $a1, 71, drawTopHorizontalLine
+            beq $a1, 76, drawTopHorizontalLine
+            beq $a1, 77, drawTopHorizontalLine
+            beq $a1, 78, drawTopHorizontalLine
+            beq $a1, 79, drawTopHorizontalLine
+            beq $a1, 80, drawTopHorizontalLine
+            beq $a1, 82, drawTopHorizontalLine
+            beq $a1, 83, drawTopHorizontalLine          
+
             add $t2, $t1, 3
             add $t3, $t0, 6                 
             add $t4, $t2, 1                 
@@ -43,19 +51,6 @@
         
         drawMiddleBottomVerticalLine:           
 
-            add $t2, $t1, 3
-            add $t3, $t0, 6                 
-            add $t4, $t2, 1 
-            add $t5, $t3, 5
-                                            
-            pushInStack($ra, $t0, $t1)
-            pushInStack($a0, $a1)
-            sendParameters($t3, $t2, $t5, $t4, BORDER_COLOR, FILLED)
-            jal drawRectangle
-            popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
-
-        drawMiddleVerticalLine:
             beq $a1, 65, drawTopHorizontalLine
             beq $a1, 67, drawTopHorizontalLine
             beq $a1, 69, drawTopHorizontalLine
@@ -69,19 +64,21 @@
             beq $a1, 83, drawTopHorizontalLine
 
             add $t2, $t1, 3
-            add $t3, $t0, 11                
-            add $t4, $t2, 1                 
+            add $t3, $t0, 6                 
+            add $t4, $t2, 1 
+            add $t5, $t3, 5
+                                            
             pushInStack($ra, $t0, $t1)
             pushInStack($a0, $a1)
-            sendParameters($t0, $t2, $t3, $t4, BORDER_COLOR, FILLED)
+            sendParameters($t3, $t2, $t5, $t4, BORDER_COLOR, FILLED)
             jal drawRectangle
             popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
+            popFromStack($ra, $t0, $t1)      
 
         drawTopHorizontalLine:
-            beq $a1, 76, dropTopHalfHorizontalLine
-            beq $a1, 77, dropTopHalfHorizontalLine
-            beq $a1, 78, dropTopHalfHorizontalLine
+            beq $a1, 76, drawMiddleLeftHorizontalLine
+            beq $a1, 77, drawMiddleLeftHorizontalLine
+            beq $a1, 78, drawMiddleLeftHorizontalLine
 
             add $t2, $t0, 1
             add $t3, $t1, 7
@@ -90,45 +87,17 @@
             sendParameters($t0, $t1, $t2, $t3, BORDER_COLOR, FILLED)
             jal drawRectangle
             popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
+            popFromStack($ra, $t0, $t1)      
 
-        dropTopHalfHorizontalLine:
-            beq $a1, 65, drawMiddleHalfHorizontalLine
-            beq $a1, 66, drawMiddleHalfHorizontalLine
-            beq $a1, 67, drawMiddleHalfHorizontalLine
-            beq $a1, 69, drawMiddleHalfHorizontalLine
-            beq $a1, 73, drawMiddleHalfHorizontalLine
-            beq $a1, 76, drawMiddleHalfHorizontalLine
-            beq $a1, 77, drawMiddleHalfHorizontalLine
-            beq $a1, 78, drawMiddleHalfHorizontalLine
-            beq $a1, 79, drawMiddleHalfHorizontalLine
-            beq $a1, 82, drawMiddleHalfHorizontalLine
-            beq $a1, 83, drawMiddleHalfHorizontalLine
-            beq $a1, 84, drawMiddleHalfHorizontalLine
-
-
-            add $t2, $t0, 1
-            add $t3, $t1, 5
-            pushInStack($ra, $t0, $t1)
-            pushInStack($a0, $a1)
-            sendParameters($t0, $t1, $t2, $t3, BORDER_COLOR, FILLED)
-            jal drawRectangle
-            popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
-
-        drawMiddleHalfHorizontalLine:       
-            beq $a1, 65, drawMiddleHorizontalLine
-            beq $a1, 66, drawMiddleHorizontalLine
-            beq $a1, 67, drawMiddleHorizontalLine
-            beq $a1, 71, drawMiddleHorizontalLine
-            beq $a1, 73, drawMiddleHorizontalLine
-            beq $a1, 76, drawMiddleHorizontalLine
-            beq $a1, 77, drawMiddleHorizontalLine
-            beq $a1, 78, drawMiddleHorizontalLine
-            beq $a1, 79, drawMiddleHorizontalLine
-            beq $a1, 80, drawMiddleHorizontalLine
-            beq $a1, 83, drawMiddleHorizontalLine
-            beq $a1, 84, drawMiddleHorizontalLine
+        drawMiddleLeftHorizontalLine:      
+            beq $a1, 67, drawMiddleRightHorizontalLine
+            beq $a1, 71, drawMiddleRightHorizontalLine
+            beq $a1, 73, drawMiddleRightHorizontalLine
+            beq $a1, 76, drawMiddleRightHorizontalLine
+            beq $a1, 77, drawMiddleRightHorizontalLine
+            beq $a1, 78, drawMiddleRightHorizontalLine
+            beq $a1, 79, drawMiddleRightHorizontalLine
+            beq $a1, 84, drawMiddleRightHorizontalLine
 
             add $t3, $t1, 5
             add $t4, $t0, 5
@@ -140,7 +109,7 @@
             popFromStack($a0, $a1)
             popFromStack($ra, $t0, $t1)
 
-        drawMiddleHorizontalLine:
+        drawMiddleRightHorizontalLine:
             beq $a1, 67, drawBottomHorizontalLine
             beq $a1, 69, drawBottomHorizontalLine
             beq $a1, 73, drawBottomHorizontalLine
@@ -154,20 +123,21 @@
             add $t3, $t1, 7
             add $t4, $t0, 5
             add $t2, $t4, 1
+            add $t5, $t1, 4
             pushInStack($ra, $t0, $t1)
             pushInStack($a0, $a1)
-            sendParameters($t4, $t1, $t2, $t3, BORDER_COLOR, FILLED)
+            sendParameters($t4, $t5, $t2, $t3, BORDER_COLOR, FILLED)
             jal drawRectangle
             popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
+            popFromStack($ra, $t0, $t1)       
 
         drawBottomHorizontalLine:
-            beq $a1, 65, drawRightVerticalLine
-            beq $a1, 77, drawRightVerticalLine
-            beq $a1, 78, drawRightVerticalLine
-            beq $a1, 80, drawRightVerticalLine
-            beq $a1, 82, drawRightVerticalLine
-            beq $a1, 84, drawRightVerticalLine
+            beq $a1, 65, drawLeftTopVerticalLine
+            beq $a1, 77, drawLeftTopVerticalLine
+            beq $a1, 78, drawLeftTopVerticalLine
+            beq $a1, 80, drawLeftTopVerticalLine
+            beq $a1, 82, drawLeftTopVerticalLine
+            beq $a1, 84, drawLeftTopVerticalLine
 
             add $t2, $t0, 10
             add $t3, $t1, 7
@@ -177,31 +147,12 @@
             sendParameters($t2, $t1, $t4, $t3, BORDER_COLOR, FILLED)
             jal drawRectangle
             popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
-
-        drawRightVerticalLine:
-            beq $a1, 67, drawBottomHalfHorizontalLine
-            beq $a1, 69, drawBottomHalfHorizontalLine
-            beq $a1, 71, drawBottomHalfHorizontalLine
-            beq $a1, 73, drawBottomHalfHorizontalLine
-            beq $a1, 76, drawBottomHalfHorizontalLine
-            beq $a1, 80, drawBottomHalfHorizontalLine
-            beq $a1, 82, drawBottomHalfHorizontalLine
-            beq $a1, 83, drawBottomHalfHorizontalLine
-            beq $a1, 84, drawBottomHalfHorizontalLine
-
-            add $t2, $t1, 7
-            add $t3, $t0, 11
-            add $t4, $t2, 1
-            pushInStack($ra, $t0, $t1)
-            pushInStack($a0, $a1)
-            sendParameters($t0, $t2, $t3, $t4, BORDER_COLOR, FILLED)
-            jal drawRectangle
-            popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
-            
-                
+            popFromStack($ra, $t0, $t1)        
+                     
          drawLeftTopVerticalLine:               
+
+            beq $a1, 73, drawLeftBottomVerticalLine
+            beq $a1, 84, drawLeftBottomVerticalLine
 
             add $t2, $t0, 6             
             add $t3, $t1, 1                 
@@ -214,6 +165,10 @@
             
         drawLeftBottomVerticalLine:             
 
+            beq $a1, 73, drawRightTopVerticalLine            
+            beq $a1, 83, drawRightTopVerticalLine
+            beq $a1, 84, drawRightTopVerticalLine
+
             add $t2, $t0, 6             
             add $t3, $t1, 1     
             add $t4, $t2, 5         
@@ -224,31 +179,15 @@
             popFromStack($a0, $a1)
             popFromStack($ra, $t0, $t1)
 
-        drawBottomHalfHorizontalLine:
-            beq $a1, 65, finish_drawing_letter
-            beq $a1, 67, finish_drawing_letter
-            beq $a1, 69, finish_drawing_letter
-            beq $a1, 73, finish_drawing_letter
-            beq $a1, 76, finish_drawing_letter
-            beq $a1, 77, finish_drawing_letter
-            beq $a1, 78, finish_drawing_letter
-            beq $a1, 79, finish_drawing_letter
-            beq $a1, 80, finish_drawing_letter
-            beq $a1, 82, finish_drawing_letter
-            beq $a1, 83, finish_drawing_letter
-            beq $a1, 84, finish_drawing_letter
-
-            add $t2, $t0, 10
-            add $t3, $t1, 5
-            add $t4, $t2, 1
-            pushInStack($ra, $t0, $t1)
-            pushInStack($a0, $a1)
-            sendParameters($t2, $t1, $t4, $t3, BORDER_COLOR, FILLED)
-            jal drawRectangle
-            popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)
-        
         drawRightTopVerticalLine:
+
+            beq $a1, 67, drawRightBottomVerticalLine
+            beq $a1, 69, drawRightBottomVerticalLine
+            beq $a1, 71, drawRightBottomVerticalLine
+            beq $a1, 73, drawRightBottomVerticalLine
+            beq $a1, 76, drawRightBottomVerticalLine
+            beq $a1, 83, drawRightBottomVerticalLine
+            beq $a1, 84, drawRightBottomVerticalLine
             
             add $t2, $t1, 7
             add $t3, $t0, 6
@@ -260,19 +199,28 @@
             popFromStack($a0, $a1)
             popFromStack($ra, $t0, $t1)
             
-        drawRightBottomVerticalLine:        
+        drawRightBottomVerticalLine:
+
+            beq $a1, 67, finish_drawing_letter
+            beq $a1, 69, finish_drawing_letter
+            beq $a1, 73, finish_drawing_letter
+            beq $a1, 76, finish_drawing_letter
+            beq $a1, 80, finish_drawing_letter
+            beq $a1, 82, finish_drawing_letter
+            beq $a1, 84, finish_drawing_letter        
             
             add $t2, $t1, 7
-            add $t3, $t0, 6
+            add $t3, $t0, 5
             add $t4, $t2, 1
-            add $t5, $t3, 5
+            add $t5, $t3, 6
             pushInStack($ra, $t0, $t1)
             pushInStack($a0, $a1)
             sendParameters($t3, $t2, $t5, $t4, BORDER_COLOR, FILLED)
             jal drawRectangle
             popFromStack($a0, $a1)
-            popFromStack($ra, $t0, $t1)        
+            popFromStack($ra, $t0, $t1)    
 
         finish_drawing_letter:
+            refreshBitmap
         
     jr $ra
